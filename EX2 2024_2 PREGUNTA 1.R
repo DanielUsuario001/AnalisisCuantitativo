9d376ab88f2c9fc973c184627d70a13f1847784b
DanielUsuario001 <a20217339@pucp.edu.pe>
2025-07-11 13:26:50 -0500

    Subiendo ...

---
+++ EX2 2024_2 PREGUNTA 1.R
@@ -0,0 +1,37 @@
+
+# Analisis Cuantitativo para la Toma de Decisiones
+# EX2 2024.2 
+
+# Pregunta 1: Series de tiempo (3 puntos)
+# Un supermercado que promueve productos orgánicos está monitoreando la demanda diaria de estos 
+# productos durante los últimos 301 días. 
+# Inicialmente, la demanda estaba alrededor de las 100 unidades diarias, pero se sospecha que ha 
+# disminuido debido a factores como la competencia de otros supermercados y el incremento en los 
+# precios.
+
+# Dentro del archivo “ventaDiaria.csv” se encuentra las unidades vendidas en los últimos 301 días,
+# y presenta la siguiente estructura:
+#   - dia: día de la venta (correlativo)
+#   - ventas: ventas para cada día
+
+# a) (0.5 puntos) Grafique la serie de tiempo para la venta a lo largo de cada día. Indique si 
+# presenta alguna tendencia y de qué tipo.
+
+# b) (0.5 puntos) Utilizando la función diff grafique la serie de tiempo para la venta a lo largo 
+# de cada día, diferenciada en un periodo. Indique si presenta alguna tendencia y de qué tipo.
+
+# c) (0.5 puntos) Grafique un correlograma para la venta a lo largo de cada día (serie sin aplicar
+# la función diff) y justifique si es factible utilizar un modelo autoregresivo de primer orden
+# sobre dicha serie.
+
+# d) (0.5 puntos) Grafique un correlograma para la serie de tiempo de la venta diferenciada en un 
+# periodo y justifique si es factible utilizar un modelo autoregresivo de primer orden sobre dicha
+# serie.
+
+# e) (1.0 punto) Plantee un modelo autoregresivo de primer orden para la serie de tiempo 
+# diferenciada en un periodo, estime los coeficientes del modelo y realice la estimación de la 
+# venta en el día 302.
+
+
+
+
